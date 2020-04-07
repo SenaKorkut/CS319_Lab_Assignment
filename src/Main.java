5bbc1d932df412b8f9f21a1f11eb22f22e3d4080
Sena Korkut <sena123korkut123@gmail.com>
2020-04-07 12:03:36 +0300

    Game is implemented in Game class.

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,23 @@ public class Main {
         ArrayOrganizer arrayOrganizer = new ArrayOrganizer();
         ArrayList<Integer> result = arrayOrganizer.concatAndSort(ArrayList1,ArrayList2,ArrayList3);
         System.out.println("Result => " + result);
+
+        Player player1 = new Player("Tom");
+        Player player2 = new Player("John");
+        Player player3 = new Player("James");
+        Player player4 = new Player("Henry");
+
+        Die die = new Die();
+        for (int i = 0; i < 5; i++){
+            player1.addScore(die.rollDie());
+            player2.addScore(die.rollDie());
+            player3.addScore(die.rollDie());
+            player4.addScore(die.rollDie());
+        }
+        System.out.println(player1);
+        System.out.println(player2);
+        System.out.println(player3);
+        System.out.println(player4);
+
     }
 }
